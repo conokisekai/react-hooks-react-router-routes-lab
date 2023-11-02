9fd06581cba2b2bc749231898c8d9a2c6a9f7b88
Conrad Kambi <kambiconrad@gmail.com>
2023-11-02 19:16:45 +0300

    Completed assignment

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,12 @@
 import React from "react";
 
 function Home() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+    <div>
+      <h1>Home Page</h1>
+      {/* Additional content for the Home page can be added here */}
+    </div>
+  );
 }
 
 export default Home;
